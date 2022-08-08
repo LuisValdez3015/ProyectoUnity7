4e58c22c1af13410467adc1d06209cf4eb1d0556
iazmoon <ijazmin98@hotmail.com>
2022-08-07 19:07:39 -0500

    LVL2 y LVL1
    falta acomodar luces del LVL1 y del LVL2 acomodo de props

--- Assets/PropsJazmin/Scripts/NewBullet.cs
+++ Assets/PropsJazmin/Scripts/NewBullet.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class NewBullet : MonoBehaviour
 {
     float moveSpeed = 20f;
 
     Rigidbody rb;
-
+    
     GameObject target;
 
     Vector3 moveDirection;
@@ -15,10 +16,25 @@ public class NewBullet : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        target = GameObject.FindGameObjectWithTag("Player");
+        var targets = GameObject.FindGameObjectsWithTag("Player").ToList();
+        GameObject nearestTarget = null;
+        float nearestDistance = 999f;
+        foreach (var target in targets)
+        {
+            var distance = Vector3.Distance(target.transform.position, transform.position);
+            if (distance < nearestDistance)
+            {
+                nearestDistance = distance;
+                nearestTarget = target;
+            }
+        }
+        if(nearestTarget == null)
+        {
+            return;
+        }
+        target = nearestTarget;
         moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
         rb.velocity = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z);
-
     }
 
     private void OnTriggerEnter(Collider other)
